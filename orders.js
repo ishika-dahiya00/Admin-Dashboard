53b84087902cfe758ab84b6128ec8fd2787ce427
ishu-00 <ishikadahiya22@gmail.com>
2023-11-28 00:25:49 +0530

    Completed WebPage with Responsiveness and Light-Dark Theme Features

---
+++ orders.js
@@ -0,0 +1,38 @@
+const Orders = [
+  {
+    productName: "An Ode",
+    productNumber: "85631",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Going Seventeen",
+    productNumber: "36378",
+    paymentStatus: "Refunded",
+    shipping: "Declined",
+  },
+  {
+    productName: "Semicolon",
+    productNumber: "49347",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  },
+  {
+    productName: "Teen,Age",
+    productNumber: "96996",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "Face The Sun",
+    productNumber: "22821",
+    paymentStatus: "Paid",
+    shipping: "Delivered",
+  },
+  {
+    productName: "You Make My Day",
+    productNumber: "56743",
+    paymentStatus: "Due",
+    shipping: "Pending",
+  }
+];
